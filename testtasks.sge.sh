b4c692992db86a43029bc710a28e261183a0715b
davidzentlermunro <David.Zentler-Munro.13@ucl.ac.uk>
2016-11-01 14:36:28 +0000

    Update testtasks.sge.sh
    Making compatible with CS HPC

--- testtasks.sge.sh
+++ testtasks.sge.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
-#$ -wd /data/uctpdtz/Julia/Status # <- working directory
+#$ -S /bin/bash
+#$ -wd /home/uctpdtz/Status # <- working directory
 #$ -o julia-test.sgelog # <- name of output file
 #$ -q batch.q # <- batch processing
 #$ -j y # <- joint output and error files
 #$ -N julia-test # <- name appearing on HPC status
 #$ -S /bin/bash # <- script language
-#$ -pe julia 4 # <- Requesting 4 processors per node
-#$ -M uctpdtz@ucl.ac.uk # <- Email address to send alerts to
-#$ -m beas # <- Email address to send alerts to
 #$ -t 1-30 # <- Number of tasks (corresponds to number of nodes, and number of minimum wage runs)
 #$ -tc 10
+#$ -l h_rt=24:0:0
+#$ -l tmem=1G 
+#$ -l h_vmem=1G 
+
+hostname
+date
 
 . /etc/profile.d/modules.sh
 module load shared sge julia/0.4.0 git/1.7.6 
 
-awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > /data/uctpdtz/Julia/Output/hosts$SGE_TASK_ID
+awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > /home/uctpdtz/Output/hosts$SGE_TASK_ID
 
-echo "calling julia again: Running Mod ${SGE_TASK_ID}"
+echo "calling julia: Running Mod ${SGE_TASK_ID}"
 
-julia --machinefile /data/uctpdtz/Julia/Output/hosts$SGE_TASK_ID /data/uctpdtz/Julia/Code/modelcentral.jl > /data/uctpdtz/Julia/Output/Results$SGE_TASK_ID.log
+julia --machinefile /home/uctpdtz/Output/hosts$SGE_TASK_ID /home/uctpdtz/HPC_Julia_Test/tasktest.jl > /home/uctpdtz/Output/Results$SGE_TASK_ID.log
