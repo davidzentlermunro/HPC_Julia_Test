13be9a8a5d1137886f6a5aca23db105d4b21606e
davidzentlermunro <David.Zentler-Munro.13@ucl.ac.uk>
2016-11-04 18:16:33 +0000

    Update testtasks.sge.sh

--- testtasks.sge.sh
+++ testtasks.sge.sh
@@ -1,12 +1,10 @@
 #$ -S /bin/bash
 #$ -wd /home/uctpdtz/Status # <- working directory
 #$ -o julia-test.sgelog # <- name of output file
-#$ -q batch.q # <- batch processing
 #$ -j y # <- joint output and error files
 #$ -N julia-test # <- name appearing on HPC status
 #$ -S /bin/bash # <- script language
 #$ -t 1-30 # <- Number of tasks (corresponds to number of nodes, and number of minimum wage runs)
-#$ -tc 10
 #$ -l h_rt=24:0:0
 #$ -l tmem=1G 
 #$ -l h_vmem=1G 
