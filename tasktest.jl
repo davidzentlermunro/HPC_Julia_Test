2ded8c74748d1ace6e8583b8bc841cc159aba695
davidzentlermunro <David.Zentler-Munro.13@ucl.ac.uk>
2016-11-08 10:17:05 +0000

    Update tasktest.jl

--- tasktest.jl
+++ tasktest.jl
@@ -2,7 +2,7 @@ println("Hello Hello. I am running on HPC and my ID is ", ENV["SGE_TASK_ID"])
 
 push!(LOAD_PATH, "/home/uctpdtz/HPC_Julia_Test")
 
-using TestModule
+@everywhere using TestModule
 #using JLD
 
 y=fsq(int(ENV["SGE_TASK_ID"]))
