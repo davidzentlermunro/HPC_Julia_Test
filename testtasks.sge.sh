7845e1c7e5a0ce1bb28e383c867f1c4350e72d9f
davidzentlermunro <David.Zentler-Munro.13@ucl.ac.uk>
2016-11-25 15:14:22 +0000

    Update testtasks.sge.sh

--- testtasks.sge.sh
+++ testtasks.sge.sh
@@ -9,6 +9,7 @@
 #$ -l tmem=4G 
 #$ -l h_vmem=4G 
 #$ -l hostname=burns*
+export JULIA_PKGDIR=/share/apps/julia-pkg
 export LD_LIBRARY_PATH=/share/apps/gcc-6.2.0/lib64:/share/apps/gcc-6.2.0/lib:/share/apps/julia-0.5/lib:$LD_LIBRARY_PATH
 echo "calling julia: Running Mod ${SGE_TASK_ID}"
 /share/apps/julia-0.5.0/bin/julia /home/uctpdtz/HPC_Julia_Test/tasktest.jl > /home/uctpdtz/Output/Results$SGE_TASK_ID.log
