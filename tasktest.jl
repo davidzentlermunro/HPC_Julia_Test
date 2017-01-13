13eebf1b744e30f71fa87b5f15e8c313febde0e5
davidzentlermunro <David.Zentler-Munro.13@ucl.ac.uk>
2017-01-13 11:28:12 +0000

    Update tasktest.jl

--- tasktest.jl
+++ tasktest.jl
@@ -1,10 +1,11 @@
 println(Pkg.Dir.path())
 println("Hello Hello. I am running on HPC and my ID is ", ENV["SGE_TASK_ID"])
 using JLD
+using FileIO
 push!(LOAD_PATH, "/home/uctpdtz/HPC_Julia_Test")
 using TestModule
 y=fsq(parse(Int64,ENV["SGE_TASK_ID"]))
 
 println("The square of ", ENV["SGE_TASK_ID"], " is ", y)
-
-save("/home/uctpdtz/Output", "solcentral", y)
+save(File(format"JLD","/home/uctpdtz/Output/tasktest$ENV["SGE_TASK_ID"]"), "solcentral", y)
+#save("/home/uctpdtz/Output", "solcentral", y)
